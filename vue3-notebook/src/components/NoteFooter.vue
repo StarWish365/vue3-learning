26cd79e6c5fc3e78c8b7065d9c5b21bfbe3a6914
chaoran <chaoranccr@gmail.com>
2024-09-02 18:47:03 +0200

    vue3 notebook

---
+++ vue3-notebook/src/components/NoteFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="footer">
+        <span>合计：{{list.length}}</span>
+        <button id="clear-tasks-button">清空任务</button>
+    </div>
+</template>
+
+<script setup>
+import { inject } from 'vue'
+const list = inject("list")
+</script>
+
+<style>
+
+</style>
